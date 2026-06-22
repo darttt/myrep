6a91a1bac69e24e6605a426f7668a4d3857866de
snoxess <tonather@student.21-school.ru>
2026-06-22 19:24:04 +0000

    Добавил Создание папки с именем от пользователя

--- scripts/myscript.sh
+++ scripts/myscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+BACKUP_DIR=$1
 echo "Привет, $User!"
 echo "Сегодняшняя дата и время:"
 date
 echo "А вот сколько свободного места на диске:"
 df -h /
+echo "Начинаю резервное копирование папки: $BACKUP_DIR"
+mkdir $BACKUP_DIR
